f698a54ef1378bea7356c3954d5dbdcf0946b687
Johnny Chang <johnnychang@DN525et3.SUNet>
2024-06-05 17:05:10 -0700

    final code

--- LLaVA_code/download_dataset.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-def download_dataset():
-    dataset = load_dataset("abhinavl/figure2code_data", split="test")
-    dataset.save_to_disk("local_figure2code_data")
-
-if __name__ == "__main__":
-    download_dataset()
